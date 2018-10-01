0bff1e6db2f94a0f3e781350eea2eaee5bf110a2
Phil Duffen <phil@duffen.co.uk>
2018-10-01 11:43:52 +0100

    Add methods:   filter_odd_squares   build_pixel_map   draw_image   save_image(input)
    Add "erlang/egd" to dependencies.

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -4,20 +4,75 @@ defmodule Identicon do
     |> hash_input
     |> pick_colour
     |> build_grid
+    |> filter_odd_squares
+    |> build_pixel_map
+    |> draw_image
+    |> save_image(input)
+  end
+
+  def save_image(image, input) do
+    File.write("#{input}.png", image)
+  end
+
+  def draw_image(%Identicon.Image{colour: colour, pixel_map: pixel_map}) do
+    image = :egd.create(250, 250)
+    fill = :egd.color(colour)
+
+    Enum.each(pixel_map, fn {start, stop} ->
+      :egd.filledRectangle(image, start, stop, fill)
+    end)
+
+    :egd.render(image)
+  end
+
+  def build_pixel_map(%Identicon.Image{grid: grid} = image) do
+    pixel_map =
+      Enum.map(grid, fn {_code, index} ->
+        horizontal = rem(index, 5) * 50
+        vertical = div(index, 5) * 50
+
+        top_left = {horizontal, vertical}
+        bottom_right = {horizontal + 50, vertical + 50}
+
+        {top_left, bottom_right}
+      end)
+
+    %Identicon.Image{image | pixel_map: pixel_map}
+  end
+
+  def filter_odd_squares(%Identicon.Image{grid: grid} = image) do
+    grid =
+      Enum.filter(grid, fn {code, _index} ->
+        rem(code, 2) == 0
+      end)
+
+    %Identicon.Image{image | grid: grid}
   end
 
   def build_grid(%Identicon.Image{hex: hex} = image) do
-  	hex
-  	|> Enum.chunk_every(3, 3, :discard)
+    grid =
+      hex
+      |> Enum.chunk_every(3, 3, :discard)
+      |> Enum.map(&mirror_row/1)
+      |> List.flatten()
+      |> Enum.with_index()
+
+    %Identicon.Image{image | grid: grid}
   end
 
-  def pick_colour(%Identicon.Image{hex: [r, g, b | _tail ]} = image) do
-  	%Identicon.Image{image | colour: {r, g, b}}
+  def pick_colour(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
+    %Identicon.Image{image | colour: {r, g, b}}
+  end
+
+  def mirror_row(row) do
+    [first, second | _tail] = row
+    row ++ [second, first]
   end
 
   def hash_input(input) do
-    hex = :crypto.hash(:md5, input)
-    |> :binary.bin_to_list
+    hex =
+      :crypto.hash(:md5, input)
+      |> :binary.bin_to_list()
 
     %Identicon.Image{hex: hex}
   end
--- mix.exs
+++ mix.exs
@@ -21,6 +21,7 @@ defmodule Identicon.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:egd, github: "erlang/egd"},
       {:ex_doc, "~> 0.12"}
     ]
   end
